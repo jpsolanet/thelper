38ee164748ef36eb300afa7960f8f8c38150a20b
Jan-Patrick Solanet <jpsolanet@gmail.com>
2020-09-16 16:22:35 -0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # thelper
+
 little tools to help tracking and working with groups
+
+Quickstart (random students)
+
+1. Create periods
+
+2. Create teachers & assign to periods
+
+3. Create students & assign to periods
--- thelper/routes.py
+++ thelper/routes.py
@@ -1,15 +1,17 @@
+"""sqlaclhemy models for tracker and class"""
 import random
 import pandas as pd
 import numpy as np
 import pdfkit
-from flask import url_for, make_response, render_template
+from flask import url_for, make_response, render_template, request
 from thelper import APP, DB, admin_views
 from thelper import models
 
 
 @APP.route("/")
 def hello():
-    return render_template("index.html", views=admin_views.admin_views)
+    periods = DB.session.query(models.Period).all()
+    return render_template("base.html", views=admin_views.admin_views, periods=periods)
 
 
 @APP.route("/bingo")
@@ -20,13 +22,22 @@ def get_bingo_card():
 
     df = pd.DataFrame(np.reshape(selection, (5, 5)), columns=list("abcde"))
     df.loc[2, "c"] = "electric noises"
-    return df.to_html()
+    return df.to_html(classes=["table table-condensed"])
 
 
 @APP.route("/periods/<period_id>")
 def get_period(period_id):
+    periods = DB.session.query(models.Period).all()
     period = DB.session.query(models.Period).get(period_id)
-    return period.generate_groups().to_html()
+    if period:
+        return render_template(
+            "class_schedule.html",
+            period=period,
+            views=admin_views.admin_views,
+            periods=periods,
+        )
+    else:
+        return "period not found"
 
 
 @APP.route("/all_periods")
@@ -34,11 +45,13 @@ def get_all():
     """Generate pdf of all periods"""
     periods = DB.session.query(models.Period).all()
     urls = [
-        f'http://localhost:5000{url_for("get_period", period_id=period.id)}'
+        f'http://{request.host}{url_for("get_period", period_id=period.id)}'
         for period in periods
     ]
     print(urls)
-    pdf = pdfkit.from_url(urls, False)
+    print(request.url)
+    options = {"javascript-delay": "3000"}
+    pdf = pdfkit.from_url(urls, False, options=options)
     response = make_response(pdf)
     response.headers["Content-Type"] = "application/pdf"
 
